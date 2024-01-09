167f98ec9854f1deed8719913283647f89b65271
JHni2 <wlgus0260@naver.com>
2024-01-09 16:12:43 +0900

    [feat] fetch chats

--- client/src/App.js
+++ client/src/App.js
@@ -4,20 +4,21 @@ import Login from './pages/Login';
 import HeaderNav from './components/Header';
 import { useContext } from 'react';
 import { AuthContext } from './context/AuthContext';
+import { ChatContextProvider } from './context/ChatContext';
 
 function App() {
   const { user } = useContext(AuthContext);
   console.log(user);
 
   return (
-    <>
+    <ChatContextProvider user={user}>
       <HeaderNav />
       <Routes>
         <Route path="/" element={user ? <Chat /> : <Login />} />
         <Route path="login" element={user ? <Chat /> : <Login />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
-    </>
+    </ChatContextProvider>
   );
 }
 
